6439c267afe70b2d101797a1168ca6dc375366af
Ulf Andrae <snh@ac6-100.bullx>
2023-06-04 19:04:47 +0000

    Ulf Andrae: Control transfer using config file

--- chase.py
+++ chase.py
@@ -1,68 +1,124 @@
 #!/usr/bin/env python3
 
 import argparse
-from cases import Cases
 import sys
 
-#########################################################################
-def set_verbosity(a) :
-  p = 0
-  if a.v != None :
-   p += len(a.v)
+from cases import Cases
 
-  if a.s != None :
-   p -= len(a.s)
-  return p
 
 #########################################################################
-def main(argv) :
+def set_verbosity(a):
+    p = 0
+    if a.v != None:
+        p += len(a.v)
 
-  parser = argparse.ArgumentParser(description='DE_330 Case Meta DataBase handler at your service')
-  parser.add_argument('-case',dest="case",required=False,default=None,
-                      help='Specify name of case(s) to work with. Use as -case case1[:case2:...:caseN]')
-  parser.add_argument('-exp',dest="exp",required=False,default=None,
-                      help='Specify name of exp(s) to work with within a case. Use as -exp exp1[:exp2:...:expN]')
-  parser.add_argument('-host',dest="host",help='Set host to check, default is current',required=False,default=None)
-  parser.add_argument('-list',action="store_true",help='List content of given case(s)',required=False,default=False)
-  parser.add_argument('-scan',action="store_true",help='Scan case for data',required=False,default=False)
-  parser.add_argument('-toc',action="store_true",help='Create TOC for give filetypes',required=False,default=False)
-  parser.add_argument('-path',dest="path",help='Path to directory with cases',required=False,default='cases')
-  parser.add_argument('-v', action='append_const', const=int, help='Increase verbosity for list command in particular')
-  parser.add_argument('-s', action='append_const', const=int, help='Decrease verbosity for list command in particular')
+    if a.s != None:
+        p -= len(a.s)
+    return p
 
 
-  # Handle arguments
-  if len(argv) == 1 :
-     parser.print_help()
-     sys.exit(1)
+#########################################################################
+def main(argv):
 
-  args = parser.parse_args()
-  case = args.case.split(':') if args.case is not None else None
+    parser = argparse.ArgumentParser(
+        description="DE_330 Case Meta DataBase handler at your service"
+    )
+    parser.add_argument(
+        "-case",
+        dest="case",
+        required=False,
+        default=None,
+        help="Specify name of case(s) to work with. Use as -case case1[:case2:...:caseN]",
+    )
+    parser.add_argument(
+        "-exp",
+        dest="exp",
+        required=False,
+        default=None,
+        help="Specify name of exp(s) to work with within a case. Use as -exp exp1[:exp2:...:expN]",
+    )
+    parser.add_argument(
+        "-host",
+        dest="host",
+        help="Set host to check, default is current",
+        required=False,
+        default=None,
+    )
+    parser.add_argument(
+        "-list",
+        action="store_true",
+        help="List content of given case(s)",
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "-scan",
+        action="store_true",
+        help="Scan case for data",
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "-toc",
+        action="store_true",
+        help="Create TOC for give filetypes",
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "-path",
+        dest="path",
+        help="Path to directory with cases",
+        required=False,
+        default="cases",
+    )
+    parser.add_argument(
+        "-v",
+        action="append_const",
+        const=int,
+        help="Increase verbosity for list command in particular",
+    )
+    parser.add_argument(
+        "-s",
+        action="append_const",
+        const=int,
+        help="Decrease verbosity for list command in particular",
+    )
 
-  if args.exp is not None and case is not None:
-       if len(case) > 1 :
-           print('Only give one case if exp is given')
-           sys.exit(1)
-       selection={k:args.exp.split(':') for k in case}
-  elif case is not None:
-       selection={k:[] for k in case}
-  else:
-       selection=[]
+    # Handle arguments
+    if len(argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-  # Construct the case structure
-  myc = Cases(selection= selection,
-              printlev= set_verbosity(args),
-              path= args.path,
-              host= args.host)
- 
+    args = parser.parse_args()
+    case = args.case.split(":") if args.case is not None else None
 
-  # Run the actions
-  if args.scan :
-    myc.scan()
-  elif args.list :
-    myc.print()
-  elif args.toc :
-    myc.toc()
+    if args.exp is not None and case is not None:
+        if len(case) > 1:
+            print("Only give one case if exp is given")
+            sys.exit(1)
+        selection = {k: args.exp.split(":") for k in case}
+    elif case is not None:
+        selection = {k: [] for k in case}
+    else:
+        selection = []
+
+    # Construct the case structure
+    myc = Cases(
+        selection=selection,
+        printlev=set_verbosity(args),
+        path=args.path,
+        host=args.host,
+    )
+
+    # Run the actions
+    if args.scan:
+        myc.scan()
+    elif args.list:
+        myc.print()
+    elif args.toc:
+        myc.toc()
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- transfer2lumi.py
+++ transfer2lumi.py
@@ -2,73 +2,159 @@
 
 #
 # Example on how to transfer data from atos to lumi.
-# Data will be temporary located under hpc-login:$SCRATCH/case/run/... 
-# 
-# So far the transfer implementation is by date-hour only
+# Data will be temporary located under hpc-login:$SCRATCH/case/run/...
 #
 # Ulf Andrae, SMHI, 2023
 #
 
-from datetime import datetime,timedelta
-from cases import Cases, hub, expand_dates, expand_times
 import os
+import sys
+from argparse import ArgumentParser
+from datetime import datetime, timedelta
+
+import yaml
+
+from cases import Cases, expand_dates, expand_times, hub
+
+REQUIRED = ("selection", "remote")
+GROUPS = {
+    "dates": ("sdate", "edate", "step"),
+    "leadtimes": ("stime", "etime", "leadtime_step"),
+}
 
 
-# Specify the case and the run
-case = 'gavle_2021' 
-run = 'deode_cy46ref'
+def check_config(config):
 
-# Define period set to None to fetch all, step defines the step between cycles
-# sdate = datetime.strptime("2021-08-16 00","%Y-%m-%d %H")
-# edate = datetime.strptime("2021-08-16 06","%Y-%m-%d %H")
-# step  = timedelta(hours=6)
-sdate = None
-edate = None
-step = None
+    for key, val in config.items():
 
-# Define leadtimes to fetch
-#stime = timedelta(hours=0)
-#etime = timedelta(hours=3)
-#leadtime_step = timedelta(days=0, hours=1, seconds=0)
-stime=None
-etime=None
-leadtime_step=None
+        missing = [x for x in REQUIRED if x not in val]
+        if len(missing) > 0:
+            print(f"Missing required items in {key}:", missing)
+            sys.exit()
+        for group, items in GROUPS.items():
+            explicit = group in val
+            found = [x for x in items if x in val]
+            if explicit and len(found) > 0:
+                print(f"Specify either {group} or {items} for {key}")
+                sys.exit()
+            elif not explicit and len(found) < 3 and len(found) > 0:
+                print(f"Specify all of {items} or {group} for {key}")
+                sys.exit()
+
+            if not explicit and len(found) == 3:
+
+                if group == "dates":
+                    sdate = datetime.strptime(val["sdate"], "%Y-%m-%d %H")
+                    edate = datetime.strptime(val["edate"], "%Y-%m-%d %H")
+                    step = step2td(val["step"])
+                    config[key][group] = expand_dates(sdate, edate, step)
+
+                if group == "leadtimes":
+                    for y in items:
+                        config[key][y] = step2td(val[y])
+                    config[key][group] = expand_times(
+                        config[key]["stime"],
+                        config[key]["etime"],
+                        config[key]["leadtime_step"],
+                    )
+
+            elif not explicit and len(found) == 0:
+                config[key][group] = []
+
+    return config
 
 
-# Specify the remote host, i.e. lumi
-remote = "lumi_transfer"
-#remote = "my_lumi_user@lumi.csc.fi"
+#########################################
 
-# Load the data 
-example = Cases(selection = { case : [run] }, printlev=0)
 
-# Construct a list of dates and times
-dates = expand_dates(sdate, edate, step)
-leadtimes = expand_times(stime, etime, leadtime_step)
+def step2td(step):
+    if isinstance(step, int):
+        return timedelta(seconds=step)
+    else:
+        (hour, minute, second) = [int(x) for x in step.split(":")]
+        return timedelta(
+            days=int(hour / 24), hours=(hour % 24), seconds=60 * minute + second
+        )
 
-for case in example.names:
-   for exp in example.cases.names:
-       x=example.cases.runs.data
-       file_template=example.cases.runs.file_templates[0]
 
-       for date in x[file_template].keys():
-           
-           if len(dates) > 0 :
-               cdate = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-               if cdate not in dates:
-                   continue
+#########################################
 
-           print(" fetch:",date)
 
-           # Parse the paths and expand the dates
-           outpath_template = example.meta[case][run][remote]["path_template"]
-           scratch_template = os.path.join(os.environ["SCRATCH"],case,exp,'%Y/%m/%d/%H/')
-           remote_outpath = hub(outpath_template,date)
-           scratch_outpath = hub(scratch_template,date)
+def transfer(cfg):
 
-           # Get a list of files
-           files = example.reconstruct(dtg=date,leadtime=leadtimes)
+    print("Config:", cfg["selection"])
 
-           # Do the actual copy from ecf to scratch and rsync to lumi, 
-           # and clean the intermediate files
-           example.transfer(files,scratch_outpath, { 'host' : remote ,'outpath' : remote_outpath })
+    # Load the metadata
+    example = Cases(selection=cfg["selection"], printlev=0, host="atos")
+
+    for case in example.names:
+        for exp in example.cases.names:
+            if cfg["remote"] not in example.meta[case][exp]:
+                print(f"  {cfg['remote']} not defined for {case}/{exp}")
+                continue
+
+            outpath_template = example.meta[case][exp][cfg["remote"]]["path_template"]
+            print(f" transfer data to:", outpath_template)
+
+            scratch_template = os.path.join(
+                os.environ["SCRATCH"], case, exp, "%Y/%m/%d/%H/"
+            )
+
+            x = example.cases.runs.data
+            file_template = example.cases.runs.file_templates[0]
+
+            for date in x[file_template].keys():
+
+                if len(cfg["dates"]) > 0:
+                    cdate = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+                    if cdate not in cfg["dates"]:
+                        continue
+
+                print(" fetch:", date)
+
+                # Parse the paths and expand the dates
+                remote_outpath = hub(outpath_template, date)
+                scratch_outpath = hub(scratch_template, date)
+
+                # Get a list of files
+                files = example.reconstruct(dtg=date, leadtime=cfg["leadtimes"])
+
+                # Do the actual copy from ecf to scratch and rsync to lumi,
+                # and clean the intermediate files
+                example.transfer(
+                    files,
+                    scratch_outpath,
+                    {"host": cfg["remote"], "outpath": remote_outpath},
+                )
+
+
+#########################################################################
+def main(argv):
+
+    parser = ArgumentParser(
+        description="DE_330 Case Meta DataBase data transfer at your service"
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        dest="config",
+        required=True,
+        default=None,
+        help="Config file for data transfers",
+    )
+
+    args = parser.parse_args()
+    config = yaml.safe_load(open(args.config))
+    config = check_config(config)
+
+    for trans, vals in config.items():
+        selection = vals["selection"]
+        show = Cases(selection=selection, printlev=0, host="atos")
+        for case in show.names:
+            for run in show.cases.names:
+                vals["selection"] = {case: run}
+                transfer(vals)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
